a3e20b2c5a9959e58120b46317e4ae03f5a22677
reyaansinghsahoo <reyaansinghsahoo@gmail.com>
2024-08-11 18:29:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,35 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("reyaan failure test")
+
+with st.form("question 1"):
+    n1=51
+    n2=34
+    s=n1+n2
+    a=s st.number_input("enter your answer", step=1)
+    if st.form_submit_buton("check awnswer"):
+        st.write("your anwswer",a)
+        st.write("actual anwswer:",s)
+        if a==s:
+            st.write("fantastic whoooohoooooooo")
+        else:
+            st.write("boo red card")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
