553b75cc7dc425b35c8edc8acebc73d4c38f8e53
reyaansinghsahoo <reyaansinghsahoo@gmail.com>
2024-08-22 10:23:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,14 @@ list1=[100,200,300,400,500,600,700,800,900,1000]
 list2=[1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
 
 st.write(list1[3])
-
+i=0
 for c in q:
     st.write(c)
 
     with st.form(str(c)):
-        n1=51
-        n2=34
+        n1=list1[i]
+        n2=list2[i]
+        i=i+1
         s=n1+n2
         st.write("what is the sum of",n1," and ",n2)
         a=st.number_input("enter your answer", step=1)
