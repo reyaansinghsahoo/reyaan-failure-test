22fe51861f7e60e208704a4e81357c4467485996
reyaansinghsahoo <reyaansinghsahoo@gmail.com>
2024-08-22 10:16:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit as st
 st.title("reyaan failure test")
 
 q=[1,2,3,4,5,6,7,8,9,10]
+list1=[100,200,300,400,500,600,700,800,900,1000]
+list2=[1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
+
+st.write(list1[0])
 
 for c in q:
     st.write(c)
